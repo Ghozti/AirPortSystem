2a90859effb4f783c300c489fe6f26143b5a3743
Ghozti <ybalderas1005@gmail.com>
2021-02-09 19:56:44 -0500

    created the data class 9will have stuff like city names, times etc

--- src/ghozti/airportsystem/flights/FlightCreator.java
+++ src/ghozti/airportsystem/flights/FlightCreator.java
@@ -1,4 +1,17 @@
 package ghozti.airportsystem.flights;
 
+import java.util.Random;
+
 public class FlightCreator {
+
+    private String[] createStringFlightParams(){
+        Random r = new Random();
+        String gate = (char)(r.nextInt(26) + 'a') + "" + (int) (Math.random() * (0 - 20)) + 20;
+        String id = (char)(r.nextInt(26) + 'a') + (int) (Math.random() * (0 - 10000)) + 10000 + "";
+        return new String[] {};
+    }
+
+    //public Flight createFlight(){
+        //return new Flight();
+    //}
 }
