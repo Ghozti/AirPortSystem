a881e38b99fdb5081f4a1cefd36bc19a68ff5580
Ghozti <ybalderas1005@gmail.com>
2021-02-09 20:34:08 -0500

    added some names

--- src/ghozti/airportsystem/Main.java
+++ src/ghozti/airportsystem/Main.java
@@ -1,7 +1,6 @@
 package ghozti.airportsystem;
 
 public class Main {
-
     public static void main(String[] args) {
 	    MainSystem mainSystem = new MainSystem();
     }
--- src/ghozti/airportsystem/data/systemData.java
+++ src/ghozti/airportsystem/data/systemData.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 public class systemData {
 
     public static ArrayList<String> cities = new ArrayList<>();
+    public static ArrayList<String> names = new ArrayList<>();
 
-    private void setCities(){
+    private static void setCities(){
         cities.add("New York");
         cities.add("London");
         cities.add("Tokyo");
@@ -28,5 +29,44 @@ public class systemData {
         cities.add("Madrid");
         cities.add("Miami");
         cities.add("Alexandria");
+    }
+
+    private static void setNames(){
+        names.add("John");
+        names.add("Marcus");
+        names.add("Porter");
+        names.add("Kian");
+        names.add("Leila");
+        names.add("Barbara");
+        names.add("Celia");
+        names.add("Nikolas");
+        names.add("Immanuel");
+        names.add("Rhianna");
+        names.add("Elisabeth");
+        names.add("Hayden");
+        names.add("Ezekiel");
+        names.add("Seth");
+        names.add("Alaina");
+        names.add("Angelo");
+        names.add("Elliot");
+        names.add("Marcos");
+        names.add("Russell");
+        names.add("Wendy");
+        names.add("Jessie");
+        names.add("Josh");
+        names.add("Ezekiel");
+        names.add("Carson");
+        names.add("Andy");
+        names.add("Britney");
+        names.add("Anthony");
+        names.add("Yesi");
+        names.add("Jhondavid");
+        names.add("Angel");
+        names.add("Jian Wei");
+        names.add("Roman");
+        names.add("Adison");
+        names.add("Trace");
+        names.add("Zain");
+        names.add("Jason");
     }
 }
