b46c070a0d7ef530bfcff1e00bef59ab20047115
Ghozti <ybalderas1005@gmail.com>
2021-02-13 15:20:07 -0500

    added some basic methods and a menu for the UserOptions class

--- src/ghozti/airportsystem/userInteractions/UserOptions.java
+++ src/ghozti/airportsystem/userInteractions/UserOptions.java
@@ -1,4 +1,36 @@
 package ghozti.airportsystem.userInteractions;
 
 public class UserOptions {
+
+    /**
+     * Menu
+     */
+
+    public static void Menu(){
+        System.out.println("[1] ADD A PASSENGER\n" +
+                "[2] REMOVE A PASSENGER\n" +
+                "[3] PRINT PASSENGERS\n" +
+                "[4] ADD A FLIGHT\n" +
+                "[5] REMOVE A FLIGHT\n" +
+                "[6] PRINT FLIGHTS");
+    }
+
+    /**
+     * Passenger Related
+     */
+    public static void addPassenger(){}
+
+    public static void removePassenger(){}
+
+    public static void printPassengers(){}
+
+    /**
+     * Flight Related
+     */
+
+    public static void addFlight(){}
+
+    public static void removeFlight(){}
+
+    public static void printFlights(){}
 }
